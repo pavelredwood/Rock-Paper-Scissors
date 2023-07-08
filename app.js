ed86a10c91248c237b6a2b5a71f8a2ce4b7cc963
Pawel Sapron <pavel.redwood@gmail.com>
2023-07-09 01:29:01 +0300

    Added format file

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-function getComputerChoice () {
-    const choices = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-};
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
 
 let playerSelection;
 
-
-function playSingleRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (
-        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-        (playerSelection === 'Scissors' && computerSelection === 'Rock')
-    ) {
-        return `Player win ${playerSelection} beats ${computerSelection}`;
-    } else {
-        return `Comp win ${computerSelection} beats ${playerSelection}`;
-    }
-};
+function playSingleRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
+  ) {
+    return `Player win ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `Comp win ${computerSelection} beats ${playerSelection}`;
+  }
+}
