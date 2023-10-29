c49801fb149bb69fa3e4fdf4ae23cb1922bb8b1f
Pavel Sapron <pavel.redwood@gmail.com>
2023-10-29 14:50:15 +0300

    Added Eslint as airbnb

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
+  const choices = ['Rock', 'Paper', 'Scissors'];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
@@ -9,22 +9,21 @@ function playSingleRound(playerSelection, computerSelection) {
   computerSelection = computerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
     return "It's a tie!";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
+  } if (
+    (playerSelection === 'rock' && computerSelection === 'scissors')
+    || (playerSelection === 'paper' && computerSelection === 'rock')
+    || (playerSelection === 'scissors' && computerSelection === 'rock')
   ) {
     return `Player win ${playerSelection} beats ${computerSelection}`;
-  } else {
-    return `Comp win ${computerSelection} beats ${playerSelection}`;
   }
+  return `Comp win ${computerSelection} beats ${playerSelection}`;
 }
 
 function game() {
-  console.log("Game begins!");
+  console.log('Game begins!');
   for (let r = 0; r < 5; r++) {
-    let playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
-    let result = playSingleRound(playerSelection, getComputerChoice());
+    const playerSelection = prompt('Enter your choice (Rock, Paper, or Scissors):');
+    const result = playSingleRound(playerSelection, getComputerChoice());
     console.log(result);
   }
 }
